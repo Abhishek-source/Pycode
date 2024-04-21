72ea80e0d5c212b266aa0e063d524a8f539e97b0
Abhishek-source <abhishek.tripathi88@gmail.com>
2024-04-21 15:20:19 +0530

    More learning

---
+++ Int.py
@@ -0,0 +1,9 @@
+# Ask user for name and remove whitespace and capitalize in full
+name= input("What's your name? ").strip().title()
+
+
+#Split user's name intro first name and last name
+first, last =name.split(" ")
+
+#Say hello to user
+print (f"hello, {first}" )
